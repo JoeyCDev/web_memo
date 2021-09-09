13b3514dc6ba87e16199c75a26e695271d0e7582
joseph58080 <joseph58080@gmail.com>
2021-09-09 13:25:46 +0900

    [최우영] - 메모 프로젝트 환경설정

---
+++ src/test/java/com/majon/memo/MemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.majon.memo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
